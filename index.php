13d6541154054789c93f44c6d513ec4951e6f9c3
stevendepaulapaiao99 <stevendepaulapaiao99@gmail.com>
2020-05-12 23:53:35 -0300

    criamos métodos de listagem

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
   require_once("config.php");
 
-  $user = new Usuario();
-  $user->loadById(2);
+  // $user = new Usuario();
+  // $user->loadById(2);
+  // echo $user;
 
-  echo $user;
+  // $lista = Usuario::getList();
+  // echo json_encode($lista);
+
+  // $busca = Usuario::search("ste");
+  // echo json_encode($busca);
+
+  $usuario = new Usuario();
+  $usuario->login("strown1", "12qw34er");
+  echo $usuario;
 
 ?>
